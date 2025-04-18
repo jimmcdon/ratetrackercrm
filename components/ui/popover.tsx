c81a3e782caeedc02a5e0442274451ade0f402ad
McD <mcd@m22.io>
2025-04-18 11:12:49 -0700

    chore: regenerate pnpm-lock.yaml

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -1,31 +1,114 @@
 "use client"
 
 import * as React from "react"
-import * as PopoverPrimitive from "@radix-ui/react-popover"
-
 import { cn } from "@/lib/utils"
 
-const Popover = PopoverPrimitive.Root
+interface PopoverContextValue {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
 
-const PopoverTrigger = PopoverPrimitive.Trigger
+const PopoverContext = React.createContext<PopoverContextValue>({
+  open: false,
+  onOpenChange: () => {},
+})
+
+interface PopoverProps {
+  open?: boolean
+  onOpenChange?: (open: boolean) => void
+  children: React.ReactNode
+}
+
+const Popover = React.forwardRef<HTMLDivElement, PopoverProps>(
+  ({ open: controlledOpen, onOpenChange, children, ...props }, ref) => {
+    const [uncontrolledOpen, setUncontrolledOpen] = React.useState(false)
+    const open = controlledOpen !== undefined ? controlledOpen : uncontrolledOpen
+
+    const handleOpenChange = React.useCallback(
+      (newOpen: boolean) => {
+        setUncontrolledOpen(newOpen)
+        onOpenChange?.(newOpen)
+      },
+      [onOpenChange]
+    )
+
+    return (
+      <PopoverContext.Provider value={{ open, onOpenChange: handleOpenChange }}>
+        <div ref={ref} {...props}>
+          {children}
+        </div>
+      </PopoverContext.Provider>
+    )
+  }
+)
+Popover.displayName = "Popover"
+
+const PopoverTrigger = React.forwardRef<
+  HTMLButtonElement,
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+>(({ ...props }, ref) => {
+  const { onOpenChange } = React.useContext(PopoverContext)
+  return (
+    <button
+      type="button"
+      onClick={() => onOpenChange(true)}
+      ref={ref}
+      {...props}
+    />
+  )
+})
+PopoverTrigger.displayName = "PopoverTrigger"
 
 const PopoverContent = React.forwardRef<
-  React.ElementRef<typeof PopoverPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Portal>
-    <PopoverPrimitive.Content
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & {
+    align?: "start" | "center" | "end"
+    sideOffset?: number
+  }
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => {
+  const { open } = React.useContext(PopoverContext)
+  const triggerRef = React.useRef<HTMLButtonElement>(null)
+  const contentRef = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        contentRef.current &&
+        !contentRef.current.contains(event.target as Node) &&
+        triggerRef.current &&
+        !triggerRef.current.contains(event.target as Node)
+      ) {
+        const { onOpenChange } = React.useContext(PopoverContext)
+        onOpenChange(false)
+      }
+    }
+
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside)
+      return () => document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [open])
+
+  if (!open) return null
+
+  return (
+    <div
       ref={ref}
-      align={align}
-      sideOffset={sideOffset}
       className={cn(
-        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out",
+        "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+        "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
+        "data-[side=bottom]:slide-in-from-top-2",
+        "data-[side=left]:slide-in-from-right-2",
+        "data-[side=right]:slide-in-from-left-2",
+        "data-[side=top]:slide-in-from-bottom-2",
         className
       )}
       {...props}
     />
-  </PopoverPrimitive.Portal>
-))
-PopoverContent.displayName = PopoverPrimitive.Content.displayName
+  )
+})
+PopoverContent.displayName = "PopoverContent"
 
 export { Popover, PopoverTrigger, PopoverContent }
